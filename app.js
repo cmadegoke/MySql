6df53f0acaf55499ab45c90e7c57098d143a0ae4
Catherine Adegoke <cmadegoke@gmail.com>
2020-02-27 15:36:51 -0500

    update

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const inquirer = require('inquirer');
 const connection = require('./Config/connection');
 
 // import functions to work with database
-const {createEmp } = require('./lib/db-items');
-const {createRole } = require('./lib/db-items');
-const {createDept } = require('./lib/db-items');
-const {view_Dept } = require('./lib/db-items');
-const {view_Role} = require('./lib/db-items');
-const {view_Emp} = require('./lib/db-items');
-const {Update_Emp} = require('./lib/db-items');
-const {Update_Emp_Role_Id} = require('./lib/db-items');
+const { createEmp } = require('./lib/db-items');
+const { createRole } = require('./lib/db-items');
+const { createDept } = require('./lib/db-items');
+const { view_Dept } = require('./lib/db-items');
+const { view_Role } = require('./lib/db-items');
+const { view_Emp } = require('./lib/db-items');
+const { Update_Emp } = require('./lib/db-items');
+const { Update_Emp_Role_Id } = require('./lib/db-items');
 
 
 
@@ -19,7 +19,7 @@ const {Update_Emp_Role_Id} = require('./lib/db-items');
 
 
 // import arrays of questions for inquirer prompts
-const { startquestions, createAddEmployees,createAddDepartments,createAddRoles} = require('./lib/prompts');
+const { startquestions, createAddEmployees, createAddDepartments, createAddRoles } = require('./lib/prompts');
 
 // function to start auction, defined to be async
 const startQuestion = async () => {
@@ -28,40 +28,50 @@ const startQuestion = async () => {
   // console.log(task)
   // depending on the answer, do an action
   if (task === 'Add employees') {
-     await addEmployees();
-    
+    await addEmployees();
+
   } else if (task === 'Add departments') {
     await addDepartments();
 
-  } 
-    else if (task === 'Add roles') {
-  await addRoles();
-    }
-    else if (task === 'View departments') {
-      await view_Dept();
-      
-    }
-    else if (task === 'View roles') {
-      await view_Role();
-      
-    }
-    else if (task === 'View employees') {
-      const emps = await view_Emp();
-      // console.log(emps)
-     
-    }
-    else if (task === 'Update employee roles') {
-      const {newPrompt, results} = await Update_Emp();
-      console.log(results)
+  }
+  else if (task === 'Add roles') {
+    await addRoles();
+  }
+  else if (task === 'View departments') {
+    await view_Dept();
 
-      const {emp} = await inquirer.prompt(newPrompt)
-      
-      const empID = results.filter(e => emp === `${emp.first_name} ${emp.last_name} ${emp.role_id}`)[0];
+  }
+  else if (task === 'View roles') {
+    await view_Role();
 
-      const role_id  = results.find(({ id }) => id === parseInt(role_id));
+  }
+  else if (task === 'View employees') {
+    await view_Emp();
 
-     
-    }
+
+  }
+  else if (task === 'Update employee roles') {
+    const { newPrompt, results } = await Update_Emp();
+    console.log(results)
+
+    const { emp } = await inquirer.prompt(newPrompt)
+    
+    const empID = results.filter(e => emp === `${e.first_name} ${e.last_name} ${e.role_id}`)[0].id;
+    
+    const { RoleId } =  await inquirer.prompt([
+      {
+        name: 'RoleId',
+        message: 'What is the New  Role ID?',
+        type: 'list',
+        choices: ['200', '300', '400', '500', '600']
+      }
+    ]);
+
+    Update_Emp_Role_Id(empID,RoleId)
+    view_Emp()
+
+
+  }
   else {
 
 
@@ -78,25 +88,24 @@ async function addEmployees() {
     last_name: lastName
   })
   console.log(createAddEmployees);
-  
+
 };
 async function addDepartments() {
-  const {Dept} = await inquirer.prompt(createAddDepartments);
+  const { Dept } = await inquirer.prompt(createAddDepartments);
   // add to the database here!
   createDept({
     dept_name: Dept,
   })
   console.log(createAddDepartments);
-  
+
 };
 async function addRoles() {
-  const {Role } = await inquirer.prompt(createAddRoles);
+  const { title, salary, department_id } = await inquirer.prompt(createAddRoles);
   // add to the database here!
   createRole({
-    Title : title,
-    Salary : salary,
-    DeptID : department_id
-
+    Title: title,
+    Salary: salary,
+    DeptID: department_id
   })
 
   console.log(createAddRoles);
